70e12bf0434ece14550cac1b918f7a75de3df5e1
Ameni <Ameni@Ameni>
2014-02-18 20:47:25 +0100

    new Commit by ameni

---
+++ src/Sprint1/dao/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sprint1.dao;
+
+/**
+ *
+ * @author Ameni
+ */
+public class NewClass {
+    
+}
+//test!!!!!!!!!!!!!
